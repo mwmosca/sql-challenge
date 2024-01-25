0e8856f2a163c077d45a0345d38151097bf34cc6
mwmosca <mwmosca93@gmail.com>
2024-01-24 19:27:18 -0500

    new file:   EmployeeSQL/analysis.sql 	new file:   EmployeeSQL/entity_relationship_diagram.svg 	new file:   EmployeeSQL/entity_relationship_diagram.txt 	new file:   EmployeeSQL/schema.sql 	new file:   data/departments.csv 	new file:   data/dept_emp.csv 	new file:   data/dept_manager.csv 	new file:   data/employees.csv 	new file:   data/salaries.csv 	new file:   data/titles.csv

---
+++ EmployeeSQL/analysis.sql
@@ -0,0 +1,65 @@
+-- 1. List the employee number, last name, first name, sex, and salary of each employee.
+SELECT 
+	e.emp_no AS "Employee Number", 
+	e.last_name AS "Last Name",
+	e.first_name AS "First Name",
+	e.sex AS "Sex",
+	s.salary AS "Salary"
+FROM employees AS e
+JOIN salaries AS s on e.emp_no = s.emp_no;
+
+
+-- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name AS "First Name", last_name AS "Last Name", hire_date AS "Hire Date"
+FROM employees
+WHERE hire_date >= '1986-01-01' AND hire_date < '1987-01-01';
+
+
+-- 3. List the manager of each department along with their department number, 
+--    department name, employee number, last name, and first name.
+SELECT 
+	d.dept_no AS "Department Number",
+	d.dept_name AS "Department Name",
+	e.emp_no AS "Employee Number",
+	e.last_name AS "Manager Last Name",
+	e.first_name AS "Manager First Name"
+FROM departments AS d
+JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
+JOIN employees AS e ON dm.emp_no = e.emp_no;
+
+
+-- 4. List the department number for each employee along with that employee’s 
+--    employee number, last name, first name, and department name.
+SELECT
+	d.dept_no AS "Department Number",
+	e.emp_no AS "Employee Number",
+	e.last_name AS "Last Name",
+	e.first_name AS "First Name",
+	d.dept_name AS "Department Name"
+FROM departments AS d
+JOIN dept_emp AS de ON d.dept_no = de.dept_no
+JOIN employees as e on de.emp_no = e.emp_no;
+
+
+-- 5. List first name, last name, and sex of each employee whose first 
+--    name is Hercules and whose last name begins with the letter B.
+SELECT first_name AS "First Name", last_name AS "Last Name", sex AS "Sex"
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+
+-- 6. List each employee in the Sales department, including 
+--    their employee number, last name, and first name.
+SELECT e.emp_no AS "Employee Number", e.last_name AS "Last Name", e.first_name AS "First Name"
+FROM employees AS e
+JOIN dept_emp AS de ON e.emp_no = de.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+
+-- 7. List each employee in the Sales and Development departments, including 
+--    their employee number, last name, first name, and department name.
+
+
+-- 8. List the frequency counts, in descending order, of all the employee 
+--    last names (that is, how many employees share each last name).
---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE IF NOT EXISTS departments(
+	dept_no CHAR(4) PRIMARY KEY,
+	dept_name VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS titles(
+	title_id CHAR(5) PRIMARY KEY,
+	title VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id CHAR(5),
+		FOREIGN KEY(emp_title_id) REFERENCES titles(title_id),
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex CHAR(1) NOT NULL,
+	hire_date DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dept_emp(
+	emp_no INT,
+		FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	dept_no CHAR(4),
+		FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY(emp_no, dept_no)
+);
+
+CREATE TABLE IF NOT EXISTS dept_manager(
+	dept_no CHAR(4),
+		FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	emp_no INT,
+		FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY(dept_no, emp_no)
+);
+
+CREATE TABLE IF NOT EXISTS salaries(
+	emp_no INT PRIMARY KEY,
+		FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	salary INT NOT NULL
+);
